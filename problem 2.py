ca03c2d7fc40c85ed884b4e7e959f54482db4d02
Family <albon.136546120855@2depedqc.ph>
2023-04-28 23:46:42 +0800

    Instruction

--- problem 2.py
+++ problem 2.py
@@ -2,3 +2,8 @@ print("Problem 02 ~ Assignment 4".center(42, "="))
 print("\033[95m=" * 42)
 print("\033[92mCMPE-103-MODULE-2-FILE-HANDLING-IN-PYTHON")
 print("\033[95m=" * 42)
+
+# write a python program that contains 20 students with their general weighted average, then, read the text file
+# and create a program that will output the name of the student which have the highest gwa,
+# including its grades in the output.
+
